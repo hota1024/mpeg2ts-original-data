e1703638e6c60cb94b1e5ca6b0bed6085fb5c85c
hota1024 <hotalog@hota1024.com>
2020-11-04 21:30:31 +0900

    ✨ sync_byteからpidまで書き込む機能を実装

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log('hello,world')
+import sophy from 'sophy'
+import { BitStream, BitView } from 'bit-buffer'
+
+const PACKET_SIZE = 188
+
+function createPacket(pid: number, payload: Buffer): Buffer {
+  const buffer = new ArrayBuffer(PACKET_SIZE)
+  const view = new BitView(buffer)
+  const stream = new BitStream(view)
+
+  stream['bigEndian'] = true
+
+  view.setBits(0, 0x47, 8)
+  view.setBits(8, 0b0, 1)
+  view.setBits(9, 0b1, 1)
+  view.setBits(10, 0b0, 1)
+  view.setBits(11, pid, 13)
+
+  return view.buffer
+}
+
+const packet = createPacket(18, Buffer.from('hello, world'))
+sophy.write('./out/test.ts', packet.toString())
